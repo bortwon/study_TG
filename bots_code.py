4a14fe9cfe7e5755713c362505275c6895e00bf9
bortw <pikanicka@gmail.cim>
2023-02-26 15:31:56 +0300

    bot for send puppy

--- bots_code.py
+++ bots_code.py
@@ -24,7 +24,7 @@ while counter < 100:
 
     if updates['result']:
         msg_from_user = updates['result'][0]['message']['text']
-        text = f'{msg_from_user} это конечно интересно, но лучше посмотри на котика :)'
+        text = f'"{msg_from_user}" это конечно интересно, но лучше посмотри на котика :)'
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
@@ -33,6 +33,46 @@ while counter < 100:
                 cat_link = cat_response.json()['file']
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+    time.sleep(1)
+    counter += 1
+
+
+# Отправляет на все сообщения пёсиков
+
+import requests
+import time
+
+
+API_URL: str = 'https://api.telegram.org/bot'
+API_CATS_URL: str = 'https://aws.random.cat/meow'
+API_DOGS_URL: str = 'https://random.dog/woof.json'
+API_FOX_URL: str = 'https://randomfox.ca/floof/'
+BOT_TOKEN: str = ''
+ERROR_TEXT: str = 'Здесь должна была быть картинка с пёсиком :('
+
+offset: int = -2
+counter: int = 0
+dog_response: requests.Response
+dog_link: str
+
+while counter < 100:
+    print('attempt =', counter)
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        msg_from_user = updates['result'][0]['message']['text']
+        text = f'"{msg_from_user}" это конечно интересно, но лучше посмотри на пёсика :)'
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            dog_response = requests.get(API_DOGS_URL)
+            if dog_response.status_code == 200:
+                dog_link = dog_response.json()['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={dog_link}')
             else:
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
     time.sleep(1)
