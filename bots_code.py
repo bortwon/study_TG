a297ed75af7659f7c210d64493e056dc70788d46
bortw <pikanicka@gmail.cim>
2023-02-26 15:16:54 +0300

    bot for send kitty: upgrade bot (send message to user)

--- bots_code.py
+++ bots_code.py
@@ -5,6 +5,7 @@
 import requests
 import time
 
+
 API_URL: str = 'https://api.telegram.org/bot'
 API_CATS_URL: str = 'https://aws.random.cat/meow'
 BOT_TOKEN: str = ''
@@ -22,12 +23,15 @@ while counter < 100:
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
+        msg_from_user = updates['result'][0]['message']['text']
+        text = f'{msg_from_user} это конечно интересно, но лучше посмотри на котика :)'
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
             cat_response = requests.get(API_CATS_URL)
             if cat_response.status_code == 200:
                 cat_link = cat_response.json()['file']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
             else:
                 requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
