d3a36868b897ad0fc0fcc890c2cf95e395437159
bortw <pikanicka@gmail.cim>
2023-02-26 18:38:24 +0300

    add env for bots

--- bots_code.py
+++ bots_code.py
@@ -4,11 +4,14 @@
 
 import requests
 import time
+import os
+import dotenv
 
+dotenv.load_dotenv()
 
-API_URL: str = 'https://api.telegram.org/bot'
-API_CATS_URL: str = 'https://aws.random.cat/meow'
-BOT_TOKEN: str = ''
+API_TG_URL: str = os.getenv('API_TG_URL')
+API_CATS_URL: str = os.getenv('API_CATS_URL')
+BOT_TOKEN: str = os.getenv('BOT_TOKEN')
 ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
 
 offset: int = -2
@@ -20,7 +23,7 @@ cat_link: str
 while counter < 100:
     print('attempt =', counter)
 
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    updates = requests.get(f'{API_TG_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         msg_from_user = updates['result'][0]['message']['text']
@@ -31,52 +34,55 @@ while counter < 100:
             cat_response = requests.get(API_CATS_URL)
             if cat_response.status_code == 200:
                 cat_link = cat_response.json()['file']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+                requests.get(f'{API_TG_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
+                requests.get(f'{API_TG_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
             else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+                requests.get(f'{API_TG_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
     time.sleep(1)
     counter += 1
 
 
 # Отправляет на все сообщения пёсиков
 
-import requests
-import time
-
-
-API_URL: str = 'https://api.telegram.org/bot'
-API_CATS_URL: str = 'https://aws.random.cat/meow'
-API_DOGS_URL: str = 'https://random.dog/woof.json'
-API_FOX_URL: str = 'https://randomfox.ca/floof/'
-BOT_TOKEN: str = ''
-ERROR_TEXT: str = 'Здесь должна была быть картинка с пёсиком :('
-
-offset: int = -2
-counter: int = 0
-dog_response: requests.Response
-dog_link: str
-
-while counter < 100:
-    print('attempt =', counter)
-
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
-
-    if updates['result']:
-        msg_from_user = updates['result'][0]['message']['text']
-        text = f'"{msg_from_user}" это конечно интересно, но лучше посмотри на пёсика :)'
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            dog_response = requests.get(API_DOGS_URL)
-            if dog_response.status_code == 200:
-                dog_link = dog_response.json()['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={dog_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
-    time.sleep(1)
-    counter += 1
+# import requests
+# import time
+# import os
+# import dotenv
+#
+# dotenv.load_dotenv()
+#
+# API_TG_URL: str = os.getenv('API_TG_URL')
+# API_CATS_URL: str = os.getenv('API_CATS_URL')
+# API_DOGS_URL: str = os.getenv('API_DOGS_URL')
+# API_FOX_URL: str = 'API_FOX_URL'
+# BOT_TOKEN: str = os.getenv('BOT_TOKEN')
+# ERROR_TEXT: str = 'Здесь должна была быть картинка с пёсиком :('
+#
+# offset: int = -2
+# counter: int = 0
+# dog_response: requests.Response
+# dog_link: str
+#
+# while counter < 100:
+#     print('attempt =', counter)
+#
+#     updates = requests.get(f'{API_TG_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+#
+#     if updates['result']:
+#         msg_from_user = updates['result'][0]['message']['text']
+#         text = f'"{msg_from_user}" это конечно интересно, но лучше посмотри на пёсика :)'
+#         for result in updates['result']:
+#             offset = result['update_id']
+#             chat_id = result['message']['from']['id']
+#             dog_response = requests.get(API_DOGS_URL)
+#             if dog_response.status_code == 200:
+#                 dog_link = dog_response.json()['url']
+#                 requests.get(f'{API_TG_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
+#                 requests.get(f'{API_TG_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={dog_link}')
+#             else:
+#                 requests.get(f'{API_TG_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+#     time.sleep(1)
+#     counter += 1
 
 
 # ======================================================
